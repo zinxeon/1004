56fa8199fff6914b59a10eea9ec5cf70e445de26
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-04 16:52:33 +0900

    아기사자 클래스 생성 배열저장, 불러오기

---
+++ src/Babe_lion.java
@@ -0,0 +1,17 @@
+public class Babe_lion {
+    public static void main(String[] args) {
+
+        // 배열에 넣기
+        int[] arr = new int[5];
+        arr[0] = 2;
+        arr[1] = 4;
+        arr[2] = 6;
+        arr[3] = 8;
+        arr[4] = 10;
+        System.out.printf("0번에는 %d가 들어있습니다.\n", arr[0]);
+        System.out.printf("1번에는 %d가 들어있습니다.\n", arr[1]);
+        System.out.printf("2번에는 %d가 들어있습니다.\n", arr[2]);
+        System.out.printf("3번에는 %d가 들어있습니다.\n", arr[3]);
+        System.out.printf("4번에는 %d가 들어있습니다.\n", arr[4]);
+    }
+}
