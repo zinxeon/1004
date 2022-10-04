38fca327b461e7b2b2c7b7ac2c7c7ae3f261698a
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-04 16:42:19 +0900

    2씩 증가_스스로

--- src/Test1.java
+++ src/Test1.java
@@ -1,9 +1,19 @@
 public class Test1 {
     public static void main(String[] args) {
 
-        // 1부터 5까지 출력
+        // 1부터 5까지 출력 (1씩 증가)
+        // 증감식 i++ 와 i+=1 은 같은 뜻.
         for(int i=0; i<5; i+=1) {
-            System.out.println(i);
+            System.out.print(i);
         }
+        System.out.println();
+
+        // 2,4,6,8,10 을 출력(2씩 증가)
+        for(int i=0; i<=10; i+=2) {
+            System.out.print(i);
+        }
+        System.out.println();
+
+
     }
 }
