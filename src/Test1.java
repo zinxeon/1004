4fb217245ec23cd3e9ed259c53f16e43e4f74bac
DESKTOP-PJ36R0M\zinxeon <zinxeon@gmail.com>
2022-10-04 16:33:51 +0900

    아기사자반

---
+++ src/Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    public static void main(String[] args) {
+
+        // 1부터 5까지 출력
+        for(int i=0; i<5; i+=1) {
+            System.out.println(i);
+        }
+    }
+}
